76aa24acda0ffc8a473ef068c61f8856ee7250f6
Nathan B <nate.physics@berkeley.edu>
2022-08-10 13:27:37 +0200

    linted code

--- hfcnn/filters.py
+++ hfcnn/filters.py
@@ -10,15 +10,13 @@ import torch.nn.functional as F
 from hfcnn import files
 import random
 
-# Defined a class to make it a bit easier to use the hydra instantiate. 
+# Defined a class to make it a bit easier to use the hydra instantiate.
 
-class Filter():
+
+class Filter:
     def __init__(
-        self,
-        fn_req_imgs: bool=False, 
-        raw_img_folder: str=False, 
-        **kwargs
-        ):
+        self, fn_req_imgs: bool = False, raw_img_folder: str = False, **kwargs
+    ):
         """A filter object used to generate filter functions.
 
         Args:
@@ -29,7 +27,6 @@ class Filter():
         self.fn_req_imgs = fn_req_imgs
         self.raw_img_folder = raw_img_folder
         self.kwargs = kwargs
-        
 
     def set_img_path(self, new_raw_img_folder: str):
         """If the raw_img_folder is not False, will add raw_img_folder to kwargs/
@@ -44,11 +41,11 @@ class Filter():
         """Returns a function that takes a row from a data frame as an input
         and returns True or False.
 
-        If a raw_img_folder is not None, will return a function 
+        If a raw_img_folder is not None, will return a function
 
         Returns:
             [Callable]
-        """                
+        """
         return None
 
 
@@ -56,20 +53,21 @@ class Filter():
 # helper functions for filters #
 ################################
 
+
 def conv2d(image, kernel, strides):
     """Uses PyTorch to convolve a kernel over a 2D image
-    
+
     >>> image = np.array([[1,2,3], [4,5,6], [7,8,9], [10,11,12]])
     >>> kernel = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1],])
     >>> conv2d(image, kernel, 2)
     array(45)
-    
+
     >>> conv2d(image, kernel, 1)
     array([45, 72])
     """
     # convert the inputs into PyTorch tensor objects
-    image = torch.tensor(np.expand_dims(image, axis=(0,1)))
-    kernel = torch.tensor(np.expand_dims(kernel, axis=(0,1)))
+    image = torch.tensor(np.expand_dims(image, axis=(0, 1)))
+    kernel = torch.tensor(np.expand_dims(kernel, axis=(0, 1)))
 
     # Convolve the arrays
     return F.conv2d(image, kernel, stride=strides, padding=0).numpy().squeeze()
@@ -84,12 +82,13 @@ def load_and_filter(row: pd.Series, raw_img_folder):
 
         raw_img_folder (str): path to the directory containing the images
     """
-    path = files.generate_file_path(row['times'], row['port'], raw_img_folder)
+    path = files.generate_file_path(row["times"], row["port"], raw_img_folder)
     image = files.import_file_from_local_cache(path)
     return image
 
+
 def check_file_available(row: pd.Series, raw_img_folder):
-    path = files.generate_file_path(row['times'], row['port'], raw_img_folder)
+    path = files.generate_file_path(row["times"], row["port"], raw_img_folder)
     return os.path.exists(path)
 
 
@@ -99,20 +98,10 @@ def check_file_available(row: pd.Series, raw_img_folder):
 
 # %%
 class Data_Selection_Filter(Filter):
-    def __init__(
-        self, 
-        raw_img_folder: str = False, 
-        int_threshold: float = 5, 
-        **kwargs
-        ):
-        super().__init__(
-            fn_req_imgs=True, 
-            raw_img_folder=raw_img_folder, 
-            **kwargs
-            )
+    def __init__(self, raw_img_folder: str = False, int_threshold: float = 5, **kwargs):
+        super().__init__(fn_req_imgs=True, raw_img_folder=raw_img_folder, **kwargs)
         self.raw_img_folder = raw_img_folder
         self.int_threshold = int_threshold
-
 
     def filter_fn(self, row: pd.Series):
         """A filter developed to exclude image data with too little heat load
@@ -126,12 +115,12 @@ class Data_Selection_Filter(Filter):
             [type]: [description]
         """
         ##########################################################
-        ##### Important parameters that impact the selection ##### 
+        ##### Important parameters that impact the selection #####
         # def the size of the kernel
         kernel_size = 24
 
         # construct a kernel to convolve over the images
-        kernel = np.ones((kernel_size, kernel_size), dtype=int) # kernel size
+        kernel = np.ones((kernel_size, kernel_size), dtype=int)  # kernel size
 
         # when integrating ignore data below this bin
         ignore_below_this_value = 7e7
@@ -139,15 +128,15 @@ class Data_Selection_Filter(Filter):
 
         # load the image file from the pandas row
         image = load_and_filter(row, self.raw_img_folder)
-        
+
         # remove values below zero convert to int
         current_image = image.astype(int).clip(min=0)
 
         # integrate
         current_image_con = conv2d(current_image, kernel, kernel_size)
         integrated = np.sum(
-            current_image_con[current_image_con > ignore_below_this_value]/1e8
-            )
+            current_image_con[current_image_con > ignore_below_this_value] / 1e8
+        )
 
         # check to see if integral is above a threshold to consider the data
         if integrated > self.int_threshold:
@@ -159,42 +148,32 @@ class Data_Selection_Filter(Filter):
         """Returns a function that takes a row from a data frame as an input
         and returns True or False.
 
-        If a raw_img_folder is not None, will return a function 
+        If a raw_img_folder is not None, will return a function
 
         Returns:
             [Callable]
-        """                
+        """
         return lambda x: self.filter_fn(x)
 
 
 class Missing_Data_Filter(Filter):
-    def __init__(
-        self, 
-        raw_img_folder: str = False,  
-        **kwargs
-        ):
-        super().__init__(
-            fn_req_imgs=True, 
-            raw_img_folder=raw_img_folder, 
-            **kwargs
-            )
+    def __init__(self, raw_img_folder: str = False, **kwargs):
+        super().__init__(fn_req_imgs=True, raw_img_folder=raw_img_folder, **kwargs)
         self.raw_img_folder = raw_img_folder
-
 
     def filter_fn(self, row: pd.Series):
         """Filters if the file is unavailable."""
         return check_file_available(row, self.raw_img_folder)
 
-
     def row_filter(self) -> Callable:
         """Returns a function that takes a row from a data frame as an input
         and returns True or False.
 
-        If a raw_img_folder is not None, will return a function 
+        If a raw_img_folder is not None, will return a function
 
         Returns:
             [Callable]
-        """                
+        """
         return lambda x: self.filter_fn(x)
 
 
@@ -202,20 +181,21 @@ class Missing_Data_Filter(Filter):
 # Filters at the program number level #
 #######################################
 
+
 def split(prog_num_list: list, ratio_list: Union[float, list]):
-    """Takes in a list of program numbers and creates sublists based on the 
+    """Takes in a list of program numbers and creates sublists based on the
     proportions list. Numbers expected to be between (0-1). If one number, x, is
-    provided, will return two sets of ratios x, and 1-x. If two are provided, 
-    will return three sets of ratios x, y, and 1 - x - y. 
+    provided, will return two sets of ratios x, and 1-x. If two are provided,
+    will return three sets of ratios x, y, and 1 - x - y.
 
     Args:
         prog_num_list (list): List of the program numbers to split
-            
-        ratio_list (list): proportion of the data to split for each sub list. 
+
+        ratio_list (list): proportion of the data to split for each sub list.
             Numbers expected to be between (0-1).
     """
-    if "PYTHONHASHSEED" in os.environ:
-        random.seed(int(os.environ["PYTHONHASHSEED" ]))
+    if "PL_GLOBAL_SEED" in os.environ:
+        random.seed(int(os.environ["PL_GLOBAL_SEED"]))
 
     if isinstance(ratio_list, float):
         ratio_list = [ratio_list]
@@ -227,7 +207,7 @@ def split(prog_num_list: list, ratio_list: Union[float, list]):
 
     # get the number of elements
     list_length = len(prog_num_list)
-    
+
     # shuffle the list
     random.shuffle(prog_num_list)
 
@@ -235,8 +215,11 @@ def split(prog_num_list: list, ratio_list: Union[float, list]):
     sub_index = [0]
     for ratio in ratio_list:
         sub_index.append(round(list_length * ratio) + sum(sub_index))
-    
+
     sub_index.append(list_length)
-    
+
     # return the list of list of sublists
-    return [prog_num_list[sub_index[i] : sub_index[i+1]] for i in range(len(sub_index) - 1)]
+    return [
+        prog_num_list[sub_index[i] : sub_index[i + 1]]
+        for i in range(len(sub_index) - 1)
+    ]
--- hfcnn/train_pipeline.py
+++ hfcnn/train_pipeline.py
@@ -5,9 +5,15 @@ from torch.utils.data import DataLoader
 from torchinfo import summary
 from typing import List
 from omegaconf import DictConfig
-from hfcnn.utils import get_logger, instantiate_list, build_default_paths, seed_everything
+from hfcnn.utils import (
+    get_logger,
+    instantiate_list,
+    build_default_paths,
+    seed_everything,
+)
 from pytorch_lightning import LightningDataModule, LightningModule, Trainer, Callback
 from pytorch_lightning.loggers import LightningLoggerBase
+
 
 def train(cfg: DictConfig, **kwargs) -> None:
 
@@ -17,9 +23,8 @@ def train(cfg: DictConfig, **kwargs) -> None:
     if cfg.data.get("seed"):
         seed_everything(cfg.data.seed)
 
-
     default_paths = build_default_paths(cfg)
-    
+
     # Convert relative ckpt path to absolute path if necessary
     ckpt_path = cfg.trainer.get("resume_from_checkpoint")
     if ckpt_path and not os.path.isabs(ckpt_path):
@@ -30,7 +35,7 @@ def train(cfg: DictConfig, **kwargs) -> None:
     ##############
     # Datamodule #
     ##############
-    
+
     ## TODO: add param files
     if cfg.datamodule.params_file_path:
         params_file_path = os.path.join(cfg.orig_wd, cfg.datamodule.params_file_path)
@@ -38,20 +43,20 @@ def train(cfg: DictConfig, **kwargs) -> None:
         params_file_path = False
 
     pin_memory = False
-    if cfg.trainer.gpus is not None and cfg.trainer.gpus != 0:
+    if cfg.trainer.accelerator is not None and cfg.trainer.devices != 0:
         pin_memory = True
 
     #  Instatiate datamodule
     datamodule: LightningDataModule = hydra.utils.instantiate(
         cfg.datamodule,
-        train_data_path=default_paths['train'],
-        val_data_path=default_paths['validation'],
-        test_data_path=default_paths['test'],
-        data_root=default_paths['raw_folder'],
+        train_data_path=default_paths["train"],
+        val_data_path=default_paths["validation"],
+        test_data_path=default_paths["test"],
+        data_root=default_paths["raw_folder"],
         params_file_path=params_file_path,
         pin_memory=pin_memory,
     )
-    
+
     # Setup the data set
     datamodule.setup()
 
@@ -64,8 +69,6 @@ def train(cfg: DictConfig, **kwargs) -> None:
 
     log.info("DataModule: %s" % datamodule)
 
-
-
     #########
     # Model #
     #########
@@ -77,18 +80,17 @@ def train(cfg: DictConfig, **kwargs) -> None:
         metrics=cfg.metric,
         input_dim=input_dim,
         output_dim=output_dim,
-        _recursive_=False, # for hydra (won't recursively instantiate criterion)
+        _recursive_=False,  # for hydra (won't recursively instantiate criterion)
     )
     log.info("Model: %s" % model)
-    
 
     # NCHW order
-    # N represents the batch dimension 
-    # C represents the channel dimension 
-    # H represents the image height (number of rows) 
+    # N represents the batch dimension
+    # C represents the channel dimension
+    # H represents the image height (number of rows)
     # and W represents the image width (number of columns)
     C, H, W = input_dim
-    
+
     summary(model, input_size=(datamodule.batch_size, C, H, W), depth=0)
     # if cfg.zeros_weight:
     #     model.zeros_all_trainable_parameters()
@@ -109,11 +111,11 @@ def train(cfg: DictConfig, **kwargs) -> None:
     if not isinstance(loggers, list):
         loggers = [loggers]
 
-    # Instantate trainer 
+    # Instantate trainer
     trainer: Trainer = hydra.utils.instantiate(
         cfg.trainer, logger=loggers, callbacks=callbacks, deterministic=deterministic
     )
-    
+
     for i, logger in enumerate(loggers):
         log.info(f"Logger {i}: {logger.__class__.__name__} instantiated.")
 
@@ -160,7 +162,7 @@ def train(cfg: DictConfig, **kwargs) -> None:
     # see: https://github.com/ashleve/lightning-hydra-template/blob/main/src/utils/utils.py
     def empty(*args, **kwargs):
         pass
-    
+
     for logger in loggers:
         log_hyperparams_ = logger.log_hyperparams
         logger.log_hyperparams = empty
@@ -169,8 +171,7 @@ def train(cfg: DictConfig, **kwargs) -> None:
     log.info("Num of train samples: %d" % len(datamodule.train_data))
     log.info("Num of validation samples: %d" % len(datamodule.val_data))
     log.info("Num of test samples: %d" % len(datamodule.test_data))
-    log.info("Train transforms: %s" % datamodule.train_data.settings['transforms'])
-
+    log.info("Train transforms: %s" % datamodule.train_data.settings["transforms"])
 
     #########
     # Train #
@@ -189,13 +190,13 @@ def train(cfg: DictConfig, **kwargs) -> None:
             if callback.best_model_score is not None:
                 best_model_score = callback.best_model_score.item()
             break
-    
+
     ########
     # Test #
     ########
 
     if cfg.data.test_strategy is None:
-        for logger in loggers: 
+        for logger in loggers:
             logger.finalize(status="FINISHED")
         return
 
@@ -203,8 +204,6 @@ def train(cfg: DictConfig, **kwargs) -> None:
     #  - None: exit
     #  - test: use test data from datamodule
     #  - train: use train data from datamodule
-
-    
 
     log.info("Test the model according to test strategy: %s" % cfg.data.test_strategy)
 
@@ -222,7 +221,7 @@ def train(cfg: DictConfig, **kwargs) -> None:
         shuffle=False,
         pin_memory=pin_memory,
         persistent_workers=bool(cfg.datamodule.num_workers),
-        num_workers=cfg.datamodule.num_workers
+        num_workers=cfg.datamodule.num_workers,
     )
     trainer.test(model, dataloaders=dataloader)
 
