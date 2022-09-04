e5ff6aed2c1e7cde6968660a003aea0502ef487c
Nathan B <nate.physics@berkeley.edu>
2022-09-04 14:57:25 +0200

    Updated prepare_data pipeline

--- hfcnn/dataset.py
+++ hfcnn/dataset.py
@@ -1,26 +1,27 @@
 from __future__ import annotations
 import pandas as pd  # needed for the df format
+import numpy as np
 from numpy import integer, issubdtype
 from typing import Callable, Tuple, Union, List
 from torch.utils.data import Dataset, DataLoader
-from torch import sqrt, Tensor
+from torch import Tensor
 from torchvision import transforms
 from hfcnn import files, filters, utils
 from mlxtend.preprocessing import standardize
 from tqdm import tqdm
-
+from hfcnn.utils import get_logger
 
 
 def check_defaults(source1, source2):
     """
-    Checks the correct default settings. 
+    Checks the correct default settings.
     """
     default_settings = {
-        'img_dir': './data/raw/',
-        'label_list': ['PC1'],
-        'transforms': None,
-        'drop_neg_values': True,
-        'norm_param': {}
+        "img_dir": "./data/raw/",
+        "label_list": ["PC1"],
+        "transforms": None,
+        "drop_neg_values": True,
+        "norm_param": {},
     }
     for setting, value in default_settings.items():
         # check to see if setting is in source1
@@ -35,24 +36,20 @@ def check_defaults(source1, source2):
 
 
 class HeatLoadDataset(Dataset):
-    def __init__(
-        self,
-        df: str or pd.DataFrame,
-        **kwargs
-    ):
+    def __init__(self, df: str or pd.DataFrame, **kwargs):
         """Creates at HeatloadDatatset object from a dataframe or link to a dataframe
 
         Args:
             df (str or pd.DataFrame): a dataframe object cotaining possible labels
             or a directory for the dataframe object and dict stored in .hkl format.
-            
-        
+
+
         kwargs: (kwargs will supersede any stored settings)
             img_dir (str): link to the data directory
             Default: ./data/raw/
 
             transforms: any transformations of the data (applied on get item)
-                Transformations are applied prior to standardization. 
+                Transformations are applied prior to standardization.
             Default: None
 
             norm_param: the standardization parameters for any data or labels.
@@ -69,12 +66,15 @@ class HeatLoadDataset(Dataset):
         """
         # if a string is passed check to see if df is .csv or .pkl
         if isinstance(df, str):
-            if df.endswith('.csv'):
-                data = pd.read_csv(df, converters={
-                    'pressure': lambda x: utils.str_to_array(x),
-                     'iota': lambda x: utils.str_to_array(x)
-                     })
-            elif df.endswith('.pkl'):
+            if df.endswith(".csv"):
+                data = pd.read_csv(
+                    df,
+                    converters={
+                        "pressure": lambda x: utils.str_to_array(x),
+                        "iota": lambda x: utils.str_to_array(x),
+                    },
+                )
+            elif df.endswith(".pkl"):
                 data = files.import_file_from_local_cache(df)
             else:
                 raise ValueError("File must be .csv or .pkl")
@@ -96,7 +96,6 @@ class HeatLoadDataset(Dataset):
             self.settings = check_defaults(kwargs, {})
         else:
             raise TypeError("Input must be a str (path) or df")
-
 
     def __len__(self) -> int:
         """Returns the number of data points in the set
@@ -120,6 +119,10 @@ class HeatLoadDataset(Dataset):
         # This is to help differentiate between timestamps and index values.
         max_number_of_samples = 9999999999
 
+        # if a list is passed then assume it is a list of idx values
+        if issubdtype(type(idx), list):
+            return [self.__getitem__(i) for i in idx]
+
         # If idx is too small to be a timestamp, find the timestamp that
         # corresponds to the index.
         if (idx <= max_number_of_samples) and issubdtype(type(idx), integer):
@@ -132,45 +135,45 @@ class HeatLoadDataset(Dataset):
 
         # load the image from the local cache
         timestamp, port = row["times"].values[0], row["port"].values[0]
-        img_path = files.generate_file_path(timestamp, port, self.settings['img_dir'])
+        img_path = files.generate_file_path(timestamp, port, self.settings["img_dir"])
         image = files.import_file_from_local_cache(img_path)
         image = Tensor(image)
-        
+
         # set any pixels below zero to zero
-        if self.settings['drop_neg_values']:
+        if self.settings["drop_neg_values"]:
             image = image.clip(min=0)
 
-                # add channel for tensor 
+        # add channel for tensor
         if image.ndim < 3:
             image = image[None, :, :]
 
         # apply any provided transformations of the data
-        if not (self.settings['transforms'] == None):
-            image = self.settings['transforms'](image)
-        
+        if not (self.settings["transforms"] == None):
+            image = self.settings["transforms"](image)
+
         # standardize the data
-        if 'image_labels' in self.settings['norm_param']:
-            img_mean = self.settings['norm_param']['image_labels'][0]
-            img_std = self.settings['norm_param']['image_labels'][1]
+        if "image_labels" in self.settings["norm_param"]:
+            img_mean = self.settings["norm_param"]["image_labels"][0]
+            img_std = self.settings["norm_param"]["image_labels"][1]
             image = transforms.Normalize(mean=(img_mean), std=(img_std))(image)
 
         # generate the labels
-        label = row[self.settings['label_list']].copy()
-        for label_name in self.settings['label_list']:
-            if label_name in self.settings['norm_param'].keys():
+        label = row[self.settings["label_list"]].copy()
+        for label_name in self.settings["label_list"]:
+            if label_name in self.settings["norm_param"].keys():
                 rawvalue = label[label_name].values
-                label[label_name] = (rawvalue - self.settings['norm_param'][label_name][0]) /\
-                    self.settings['norm_param'][label_name][1]
+                label[label_name] = (
+                    rawvalue - self.settings["norm_param"][label_name][0]
+                ) / self.settings["norm_param"][label_name][1]
 
         label = label.to_numpy()
         if len(label) == 1:
             label = label.flatten()
-        
+
         label = Tensor(label)
 
-
         # return sample
-        sample = {'image': image, 'label': label}
+        sample = {"image": image, "label": label}
         return sample
 
     def apply(self, filter_fn: Union[Callable, list]) -> HeatLoadDataset:
@@ -182,19 +185,23 @@ class HeatLoadDataset(Dataset):
             filter_fn (Callable or list): A Callable (or list of Callables) designed to take in a row from
             the self.img_labels dataframe (pd.Series) and return a Boolean.
         """
+
+        log = get_logger(__name__)
+
         if isinstance(filter_fn, Callable):
             filter_fn = [filter_fn]
         num_of_filters = len(filter_fn)
 
         tqdm.pandas()
         temp_df = self.img_labels.copy()
-        for i, filter in enumerate(filter_fn):  
-            print(f'Applying filter {i+1}/{num_of_filters}:')
+        for i, filter in enumerate(filter_fn):
+
+            log.info(f"Applying filter {i+1}/{num_of_filters}:")
 
             # Pass the img path to the filter
-            filter.set_img_path(self.settings['img_dir'])
+            filter.set_img_path(self.settings["img_dir"])
 
-            # apply the filter to the dataframe 
+            # apply the filter to the dataframe
             filter_for_df = temp_df.progress_apply(filter.row_filter(), axis=1)
 
             # save the file
@@ -202,8 +209,7 @@ class HeatLoadDataset(Dataset):
         return HeatLoadDataset(temp_df, **self.settings)
 
     def program_nums(self):
-        """Returns the unique program numbers from the data set
-        """
+        """Returns the unique program numbers from the data set"""
         return self.img_labels["program_num"].unique()
 
     def to_file(self, path_to_file: str) -> None:
@@ -212,15 +218,17 @@ class HeatLoadDataset(Dataset):
         Args:
             path_to_file ([type]): path to file (should end in .hkl)
         """
+        log = get_logger(__name__)
+
         # construct the dict
         export_data = self.settings
-        export_data['img_labels'] = self.img_labels
+        export_data["img_labels"] = self.img_labels
 
         files.export_data_to_local_cache(
             export_data,
             path_to_file,
         )
-        print("Export Complete")
+        log.info("Export Complete")
 
     def split_by_program_num(self, prog_num_list: List[int]) -> HeatLoadDataset:
         """Generates a new copy of the data set with the subset of data that
@@ -238,7 +246,7 @@ class HeatLoadDataset(Dataset):
 
     def validation_split(self, ratio: float) -> Tuple[HeatLoadDataset, HeatLoadDataset]:
         """Takes in a training ratio for train/validation(or test) and returns
-        two HeatLoadDatasets with the corresponding ratios of programs. 
+        two HeatLoadDatasets with the corresponding ratios of programs.
 
         Args:
             ratio (float): Ratio of the validation(train). (0-1)
@@ -249,8 +257,10 @@ class HeatLoadDataset(Dataset):
         # genarate the two sets of program numbers to use
         program_num_split = filters.split(self.program_nums(), ratio)
 
-        # generate two datasets from the program numbers. 
-        return self.split_by_program_num(program_num_split[1]), self.split_by_program_num(program_num_split[0])
+        # generate two datasets from the program numbers.
+        return self.split_by_program_num(
+            program_num_split[1]
+        ), self.split_by_program_num(program_num_split[0])
 
     def normalize_data(self) -> Tuple[float, float]:
         """Calculates the normalization parameters of the dataset across all
@@ -261,33 +271,26 @@ class HeatLoadDataset(Dataset):
         Returns:
             float: mean and std
         """
-        # get the total number of pixels in the entire data set
-        _, x, y = self.__getitem__(1)["image"].size()
-        num_of_pixels = self.__len__() * x * y
-
-        # set the batch size
-        bs = min(50, self.__len__())
-
-        # set up dataloader
-        temploader = DataLoader(self, batch_size=bs)
+        log = get_logger(__name__)
 
         # solve for mean
-        total_sum = 0
-        print('Calculating mean of dataset')
-        for batch in tqdm(temploader):
-            total_sum += batch["image"].sum()
-        mean = total_sum / num_of_pixels
-        mean = mean.item()
+        tqdm.pandas()
+        log.info("Calculating mean of dataset")
+
+        sums = self.img_labels.progress_apply(lambda x: self.image_mean(x), axis=1)
+        sums = sums / self.__len__()
+        mean = sums.sum().item()
 
         # solve for std
-        print('Calculating std. of dataset')
-        sum_of_squared_error = 0
-        for batch in tqdm(temploader):
-            sum_of_squared_error += ((batch["image"] - mean).pow(2)).sum()
-        std = sqrt(sum_of_squared_error / num_of_pixels)
+        log.info("Calculating std. of dataset")
+        sum_of_squared_error = self.img_labels.progress_apply(
+            lambda x: self.image_sum_squared_error(x, mean), axis=1
+        )
+        sum_of_squared_error = sum_of_squared_error.sum()
+        std = np.sqrt(sum_of_squared_error / self.__len__())
         std = std.item()
 
-        self.settings['norm_param']['image_labels'] = (mean, std)
+        self.settings["norm_param"]["image_labels"] = (mean, std)
 
         return mean, std
 
@@ -298,16 +301,21 @@ class HeatLoadDataset(Dataset):
         Args:
             labels (list[str]): List of strings that reprepset the labels of the df.
         """
-        norm_data = standardize(self.img_labels[labels], columns=labels, return_params=True)
+        norm_data = standardize(
+            self.img_labels[labels], columns=labels, return_params=True
+        )
 
         for label in labels:
-            self.settings['norm_param'][label] = (norm_data[1]['avgs'][label], norm_data[1]['stds'][label])
+            self.settings["norm_param"][label] = (
+                norm_data[1]["avgs"][label],
+                norm_data[1]["stds"][label],
+            )
 
     def import_settings(self, settings: dict) -> None:
         """Imports the settings.
 
         Args:
-            settings (dict): self.settings from a Heatloaddataset object. 
+            settings (dict): self.settings from a Heatloaddataset object.
         """
         self.settings = settings
 
@@ -317,7 +325,7 @@ class HeatLoadDataset(Dataset):
         Returns:
             int: output dimension
         """
-        return self.__getitem__(0)['label'].size(dim=0)
+        return self.__getitem__(0)["label"].size(dim=0)
 
     def get_input_dim(self) -> List[int]:
         """Returns the input dimension of the dataset.
@@ -325,4 +333,33 @@ class HeatLoadDataset(Dataset):
         Returns:
             int: input dimension
         """
-        return list(self.__getitem__(0)['image'].size())
+        return list(self.__getitem__(0)["image"].size())
+
+    def image_mean(self, row: pd.Series):
+        """Sum over the pixels in the image."""
+        # load the image file from the pandas row
+        data = self.__getitem__(row["times"])
+        image = data["image"]
+
+        # remove values below zero convert to int
+        current_image = image.numpy().astype(np.int64).clip(min=0)
+
+        # sum the pixels
+        image_mean = np.sum(current_image) / current_image.size
+
+        return image_mean
+
+    def image_sum_squared_error(self, row: pd.Series, mean: float):
+        """Sum of squared error of the pixels of the image."""
+        # load the image file from the pandas row
+        data = self.__getitem__(row["times"])
+        image = data["image"]
+
+        # remove values below zero convert to int
+        current_image = image.numpy().astype(np.int64).clip(min=0)
+
+        # sum of squared error
+        sum_of_squared_error = np.square(current_image - mean).sum()
+        sum_of_squared_error = sum_of_squared_error / current_image.size
+
+        return sum_of_squared_error
---
+++ prepare_data.py
@@ -0,0 +1,43 @@
+import dotenv
+import hydra
+from omegaconf import DictConfig, OmegaConf
+
+# load environment variables from `.env` file if it exists
+# recursively searches for `.env` in all folders starting from work dir
+dotenv.load_dotenv(override=True)
+
+OmegaConf.register_new_resolver("dict", lambda x, y: x.get(y))
+
+
+@hydra.main(version_base="1.2", config_path="configs/", config_name="prepare.yaml")
+def main(cfg: DictConfig):
+
+    # Imports should be nested inside @hydra.main to optimize tab completion
+    # Read more here: https://github.com/facebookresearch/hydra/issues/934
+    from hfcnn import utils
+    import os
+    from hfcnn.preprocessing import prepare_data, prepare_test_data
+    from hfcnn.datamodules.heat_load_data import HeatLoadDataModule
+
+    if cfg.ignore_warnings:
+        utils.disable_warnings()
+
+    if cfg.data.seed is not None:
+        utils.seed_everything(cfg.data.seed)
+
+    if cfg.orig_wd is not None:
+        os.environ["OWD"] = cfg.orig_wd
+
+    # Applies optional utilities
+    utils.extras(cfg)
+
+    if cfg.prepare_test == True:
+        prepare_test_data(cfg)
+
+    return prepare_data(cfg)
+
+    #  Start action (or list of actions)
+
+
+if __name__ == "__main__":
+    main()
