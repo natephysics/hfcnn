e007929001ac977dcf4c61c899a50573c780a3e9
Nathan B <nate.physics@berkeley.edu>
2022-09-14 16:47:10 +0200

    Misc updates

--- train.py
+++ train.py
@@ -1,7 +1,10 @@
 import dotenv
 import hydra
 from omegaconf import DictConfig, OmegaConf
+from numpy import set_printoptions
 
+# for exporting the data
+set_printoptions(linewidth=100000)
 
 # load environment variables from `.env` file if it exists
 # recursively searches for `.env` in all folders starting from work dir
@@ -9,7 +12,8 @@ dotenv.load_dotenv(override=True)
 
 OmegaConf.register_new_resolver("dict", lambda x, y: x.get(y))
 
-@hydra.main(version_base='1.2', config_path="configs/", config_name="train.yaml")
+
+@hydra.main(version_base="1.2", config_path="configs/", config_name="train.yaml")
 def main(cfg: DictConfig):
 
     # Imports should be nested inside @hydra.main to optimize tab completion
@@ -26,7 +30,7 @@ def main(cfg: DictConfig):
         utils.seed_everything(cfg.data.seed)
 
     if cfg.orig_wd is not None:
-        os.environ['OWD'] = cfg.orig_wd
+        os.environ["OWD"] = cfg.orig_wd
 
     # Applies optional utilities
     utils.extras(cfg)
@@ -34,6 +38,7 @@ def main(cfg: DictConfig):
     return train(cfg)
 
     #  Start action (or list of actions)
-    
+
+
 if __name__ == "__main__":
     main()
